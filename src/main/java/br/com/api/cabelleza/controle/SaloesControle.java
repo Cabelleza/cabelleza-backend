3c51972103a007a456b5e5a384d5ea825cc0b47e
Diogo <cntt.dg@gmail.com>
2023-07-01 09:42:35 -0300

    Inserção de Cors

--- src/main/java/br/com/api/cabelleza/controle/SaloesControle.java
+++ src/main/java/br/com/api/cabelleza/controle/SaloesControle.java
@@ -2,6 +2,7 @@ package br.com.api.cabelleza.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import br.com.api.cabelleza.modelo.SaloesModelo;
 import br.com.api.cabelleza.servico.SaloesServico;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class SaloesControle {
     
     @Autowired
